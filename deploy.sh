68f66d86400ac2df0ececf9258cfffb87429bf50
kaangiray26 <kaangiray26@protonmail.com>
2023-06-28 14:53:29 +0200

    deploy via script

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ set -e
 vite build
 
 # Capacitor sync
-echo "\nSyncing to Android..."
+printf "\nSyncing to Android..."
 npx cap sync
 
 # Minify extractors
-echo "\nMinifying extractors..."
+printf "\nMinifying extractors..."
 search_dir=./extractors
 for entry in "$search_dir"/*.js
 do
@@ -20,7 +20,7 @@ do
 done
 
 # Push to git
-echo "\nPushing to git..."
+printf "\nPushing to git..."
 git add -A
 git commit -m "deploy via script"
 git push
