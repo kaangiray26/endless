6df5cb5aed0f47996f5313caa70223951f6964ee
kaangiray26 <kaangiray26@protonmail.com>
2023-06-28 14:52:52 +0200

    deploy via script

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,22 @@ set -e
 vite build
 
 # Capacitor sync
+echo "\nSyncing to Android..."
 npx cap sync
 
 # Minify extractors
+echo "\nMinifying extractors..."
 search_dir=./extractors
 for entry in "$search_dir"/*.js
 do
   tmp="${entry#*extractors/}"
   fname="${tmp%.*}"
-  echo Compressing "$fname".js
+  echo "$fname"
   ./node_modules/.bin/terser ./extractors/"$fname".js --compress --output=./min/"$fname".min.js
 done
 
 # Push to git
+echo "\nPushing to git..."
 git add -A
 git commit -m "deploy via script"
 git push
