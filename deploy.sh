6c776c2f71564922e06a8011c8799125f25ce870
kaangiray26 <kaangiray26@protonmail.com>
2023-06-29 09:51:32 +0200

    deploy via script

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,20 @@ do
   tmp="${entry#*extractors/}"
   fname="${tmp%.*}"
   echo "$fname"
-  ./node_modules/.bin/terser ./extractors/"$fname".js --compress --output=./min/"$fname".min.js
+  ./node_modules/.bin/terser ./extractors/"$fname".js --compress --output=./docs/min/"$fname".min.js
 done
+
+# Copy README to docs
+printf "\nCopying README to docs...\n"
+cp README.md ./docs/
+
+# Copy list.json to docs
+printf "\nCopying list.json to docs...\n"
+cp list.json ./docs/
+
+# Copy docker-compose.yml to docs
+printf "\nCopying docker-compose.yml to docs...\n"
+cp server/docker-compose.yml ./docs/server/
 
 # Push to git
 printf "\nPushing to git...\n"
