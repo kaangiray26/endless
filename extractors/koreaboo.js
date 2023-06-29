41df98ed6f4263d02c0ebf19a5356ce4f601283a
kaangiray26 <kaangiray26@protonmail.com>
2023-06-29 15:08:33 +0200

    deploy via script

--- extractors/koreaboo.js
+++ extractors/koreaboo.js
@@ -16,7 +16,7 @@ const extractor = () => {
                 url: "https://www.koreaboo.com/news" + item.querySelector("a").pathname,
                 id: item.querySelector("a").pathname,
                 dt: item.querySelector("time").dateTime,
-                image: item.querySelector("img").currentSrc,
+                image: item.querySelector("img").dataset.srcset.split(",").slice(-1)[0].split(" ")[0],
                 page: "/discover/koreaboo" + item.querySelector("a").pathname
             })))
     }
