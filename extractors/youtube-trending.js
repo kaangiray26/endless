d47ac4790fdf5f931994cd06e072b45bc864c3ec
kaangiray26 <kaangiray26@protonmail.com>
2023-06-29 17:13:46 +0200

    deploy via script

--- extractors/youtube-trending.js
+++ extractors/youtube-trending.js
@@ -10,17 +10,18 @@ const extractor = () => {
             .then(dom => [...dom.querySelectorAll("script")])
             .then(scripts => scripts.filter(script => script.text.startsWith('var ytInitialData')).pop())
             .then(script => JSON.parse(new Function(script.innerHTML + "return ytInitialData")()))
-            .then(data => data.contents.singleColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents)
+            .then(data => data.contents.singleColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents.slice(0, -1))
+            .then(items => items.map(item => item.itemSectionRenderer.contents[0]))
+            .then(items => items.filter(item => item.videoWithContextRenderer))
             .then(items => items.map(item => ({
-                title: item.itemSectionRenderer.contents[0].videoWithContextRenderer.headline.runs[0].text,
-                author: item.itemSectionRenderer.contents[0].videoWithContextRenderer.shortBylineText.runs[0].text,
-                url: "https://m.youtube.com/watch?v=" + item.itemSectionRenderer.contents[0].videoWithContextRenderer.navigationEndpoint.watchEndpoint.videoId,
-                id: item.itemSectionRenderer.contents[0].videoWithContextRenderer.navigationEndpoint.watchEndpoint.videoId,
+                title: item.videoWithContextRenderer.headline.runs[0].text,
+                author: item.videoWithContextRenderer.shortBylineText.runs[0].text,
+                url: "https://m.youtube.com/watch?v=" + item.videoWithContextRenderer.navigationEndpoint.watchEndpoint.videoId,
+                id: item.videoWithContextRenderer.navigationEndpoint.watchEndpoint.videoId,
                 dt: null,
-                image: item.itemSectionRenderer.contents[0].videoWithContextRenderer.thumbnail.thumbnails.slice(-1)[0].url,
-                page: "/discover/youtube/" + item.itemSectionRenderer.contents[0].videoWithContextRenderer.navigationEndpoint.watchEndpoint.videoId,
+                image: item.videoWithContextRenderer.thumbnail.thumbnails.slice(-1)[0].url,
+                page: "/discover/youtube/" + item.videoWithContextRenderer.navigationEndpoint.watchEndpoint.videoId,
             })))
-            .catch(err => []);
     }
 
     async function get_post(id, request = null, args = null) {
