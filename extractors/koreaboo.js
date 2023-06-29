897d25f27ea744099297e8f4f8e727af96eab9d9
kaangiray26 <kaangiray26@protonmail.com>
2023-06-29 15:03:10 +0200

    deploy via script

--- extractors/koreaboo.js
+++ extractors/koreaboo.js
@@ -16,13 +16,13 @@ const extractor = () => {
                 url: "https://www.koreaboo.com/news" + item.querySelector("a").pathname,
                 id: item.querySelector("a").pathname,
                 dt: item.querySelector("time").dateTime,
-                image: item.querySelector("img").src,
+                image: item.querySelector("img").currentSrc,
                 page: "/discover/koreaboo" + item.querySelector("a").pathname
             })))
     }
 
     async function get_post(id, request = null, args = null) {
-        //
+        return null
     }
 
     return {
