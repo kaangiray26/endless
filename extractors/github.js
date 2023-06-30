e02c381b8582bcacffff4dd41496491f27f0257c
kaangiray26 <kaangiray26@protonmail.com>
2023-06-30 18:40:12 +0200

    deploy via script

--- extractors/github.js
+++ extractors/github.js
@@ -4,7 +4,18 @@ const extractor = () => {
     }
 
     async function get_post(id, request = null, args = null) {
-        //
+        return await fetch("https://github.com/" + id)
+            .then(res => res.text())
+            .then(str => new DOMParser().parseFromString(str, "text/html"))
+            .then(dom => ({
+                title: dom.querySelector("meta[property='og:title']").content,
+                author: null,
+                url: "https://github.com/" + id,
+                id: id,
+                dt: null,
+                image: dom.querySelector("meta[property='og:image']").content,
+                page: "/discover/github" + id,
+            }))
     }
 
     return {
