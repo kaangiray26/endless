f068b87f7d107f3b40ba3ceff89fdc69436fd8fe
kaangiray26 <kaangiray26@protonmail.com>
2023-06-30 16:41:29 +0200

    deploy via script

--- extractors/youtube-trending.js
+++ extractors/youtube-trending.js
@@ -24,28 +24,37 @@ const extractor = () => {
                     page: "/discover/youtube/" + item.videoWithContextRenderer.navigationEndpoint.watchEndpoint.videoId,
                 })))
         }
+        return [];
 
-        return await fetch("https://m.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false", {
-            method: "POST",
-            headers: {
-                UserAgent: "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
-                "X-Goog-EOM-Visitor-Id": ""
-            },
-            body: JSON.stringify({
-                context: {
-                    client: {
-                        clientName: "MWEB",
-                        clientVersion: "2.20230627.03.00"
-                    }
-                },
-                continuation: "4qmFsgKbAhIKRkV0cmVuZGluZxr0AUNCQjZzZ0ZIU1RKNk5EWkVialpRT0VOTloyOUpkRFZsU2w5ZlNFcHBOekE1VjIxWlMxcEJiMXBsV0ZKbVkwZEdibHBXT1hwaWJVWjNZekpvZG1SR09YbGFWMlJ3WWpJMWFHSkNTV1pYVkd4UFYyNXdRMVpITVcxa2VtaGFaREIwUkdWclJuUlJNVUo2WTIxU2NVMUhTalJQUjNSRFduaHZiVUZCUW14aVowRkNVa1ZWUVVGVlVrWkJRVVZCVW10V01HTnRWblZhUjJ4MVduZEJRa0ZSU1VKQlVVRkNRVUZCUWtGUlJEWnVUV1U1UTFGSlNVVkKaAhVicm93c2UtZmVlZEZFdHJlbmRpbmc%3D"
-            })
-        })
-            .then(res => res.json())
+        // return await fetch("https://m.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false", {
+        //     method: "POST",
+        //     headers: {
+        //         UserAgent: "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
+        //         "X-Goog-EOM-Visitor-Id": ""
+        //     },
+        //     body: JSON.stringify({
+        //         context: {
+        //             client: {
+        //                 clientName: "MWEB",
+        //                 clientVersion: "2.20230627.03.00"
+        //             }
+        //         },
+        //         continuation: "4qmFsgKbAhIKRkV0cmVuZGluZxr0AUNCQjZzZ0ZIU1RKNk5EWkVialpRT0VOTloyOUpkRFZsU2w5ZlNFcHBOekE1VjIxWlMxcEJiMXBsV0ZKbVkwZEdibHBXT1hwaWJVWjNZekpvZG1SR09YbGFWMlJ3WWpJMWFHSkNTV1pYVkd4UFYyNXdRMVpITVcxa2VtaGFaREIwUkdWclJuUlJNVUo2WTIxU2NVMUhTalJQUjNSRFduaHZiVUZCUW14aVowRkNVa1ZWUVVGVlVrWkJRVVZCVW10V01HTnRWblZhUjJ4MVduZEJRa0ZSU1VKQlVVRkNRVUZCUWtGUlJEWnVUV1U1UTFGSlNVVkKaAhVicm93c2UtZmVlZEZFdHJlbmRpbmc%3D"
+        //     })
+        // })
+        //     .then(res => res.json())
     }
 
     async function get_post(id, request = null, args = null) {
-        return null
+        return {
+            title: "This function is not implemented yet",
+            author: "YouTube",
+            url: "https://m.youtube.com/watch?v=" + id,
+            id: id,
+            dt: null,
+            image: "/images/bitmap_dark.png",
+            page: "/discover/youtube/" + id,
+        }
     }
 
     return {
